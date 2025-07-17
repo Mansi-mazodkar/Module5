24d549aa1a081e1d8e752cf1581517f394be54c7
Mansi-mazodkar <mansi24@navgurukul.org>
2025-07-17 19:50:51 +0530

    Task 2 half completed

--- task 1/manipulatingelement.js
+++
@@ -1,9 +0,0 @@
-// Date = 7/11/25
-
-// createElement()
-
-// 1) Creating a new div
-
-let div = document.createElement('div');
-div.innerHTML = '<p>CreateElement example</p>';
-document.body.appendChild(div);
---
+++ task 2/Task2.js
@@ -0,0 +1,25 @@
+const background = document.createElement("div");
+background.className = "background";
+background.style.backgroundImage = "url('https://i.ibb.co/RbV2JPr/chatbg-1.png')";
+background.style.backgroundSize = "cover";
+background.style.height = "100vh";
+
+const heading = document.createElement("h1");
+heading.className = "heading";
+heading.textContent = "Chat";
+heading.style.color = "#ffffff";
+heading.style.backgroundColor = "#323f4b";
+heading.style.textAlign = "center";
+heading.style.fontSize = "20px";
+heading.style.fontFamily = "Roboto";
+heading.style.fontWeight = "bold";
+heading.style.padding = "14px";
+
+const whole = document.createElement("div");
+whole.className = "whole";
+whole.style.padding = "14px";
+whole.style.color = "#ffffff";
+whole.style.fontFamily = "Roboto";
+whole.style.fontSize = "12px";
+
+const p1 = document.createElement("p")
